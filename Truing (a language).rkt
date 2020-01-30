d3c4d312a213029a297ff1fec81581b9f4e51800
Huy Vo <voxqhuy@gmail.com>
2020-01-30 15:41:01 -0500

    add TruingExpr

--- Truing (a language).rkt
+++ Truing (a language).rkt
@@ -1,2 +1,11 @@
 #lang plait
 
+(print-only-errors #t)
+
+; abstract syntax for Truing language
+
+(define-type TruingExpr
+  (truing-literal [data : String]) ; literal
+  (truing-append [lhs : TruingExpr] [rhs : TruingExpr]) ; (string-append a b)
+  (truing-reverse [expr : TruingExpr])) ; (rope-reverse s)
+
