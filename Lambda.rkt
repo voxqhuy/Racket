9ce219a121bb641932c302401ffe6a9018b9c771
Huy Vo <voxqhuy@gmail.com>
2020-03-11 20:52:16 -0400

    update

--- Lambda.rkt
+++ Lambda.rkt
@@ -6,7 +6,7 @@
 ; Abstract syntax for Curly language
 (define-type Value
   (numV  [n : Number])
-  (closV [arg  : (Listof Symbol)]
+  (closV [params : (Listof Symbol)]
          [body : Exp]
          [env  : Env]))
 
@@ -17,16 +17,19 @@
          [r : Exp])
   (multE [l : Exp]
          [r : Exp])
-  (letE  [n : Symbol] 
-         [rhs : Exp]
+  (letE  [ids  : (Listof Symbol)] 
+         [exps : (Listof Exp)]
          [body : Exp])
-  (lamE  [n : (Listof Symbol)]
+  (letE* [ids  : (Listof Symbol)] 
+         [exps : (Listof Exp)]
          [body : Exp])
-  (appE  [fun : Exp]
-         [arg : Exp]))
+  (lamE  [params   : (Listof Symbol)]
+         [body : Exp])
+  (appE  [fun  : Exp]
+         [args : (Listof Exp)]))
 
 (define-type Binding
-  (bind [name : (Listof Symbol)]
+  (bind [name : Symbol]
         [val  : Value]))
 
 (define-type-alias Env (Listof Binding))
@@ -52,23 +55,31 @@
   (test (s-exp-list->symbol-list (list `x `y)) (list 'x 'y))
   (test (s-exp-list->symbol-list (list `x `y `z)) (list 'x 'y 'z)))
 
-; Symbol (Listof Symbol) -> Boolean
-; check if a list contains a specific symbol
-; given: 'a (list 'x 'y)     expected: false
-; given: 'x (list 'x 'y 'z)  expected: true
-(define (contains? [s : Symbol] [lst : (Listof Symbol)]) : Boolean
-  (cond
-    [(empty? lst) #f]
-    [(cons?  lst) (or (equal?    s (first lst))
-                      (contains? s (rest  lst)))]))
 
-; TESTS for contains?
-(module+ test
-  (test (contains? 'a empty)           #f)
-  (test (contains? 'a (list 'x))       #f)
-  (test (contains? 'x (list 'x 'y))    #t)
-  (test (contains? 'z (list 'x 'y 'z)) #t))
-
+; concrete syntax for Curly language
+; 5
+; x
+; {+ 2 3}
+; {* 2 3}
+; {let {[x {+ 1 2}]} y}
+; {let* {[y 3]} x}
+; {add-all 2 4 5 7}
+; {lambda {x} 9}
+; {let* {[x 5]} {let {[y 6]} x}}
+; {let {[add-x {lambda {y} {+ x y}}] [x 2] {add-x 3})
+; (E)BNF (Extended) Backus-Naur Form
+; <expression> ::= <number> | <id> | <plus> | <multiply>
+;                  <let> | <let*> | <lambda> | <call>
+; <number>     ::= 0 | 1 | 2 ...
+; <character>  ::= "A" | "B" | "C" ... "a" | "b" | "c" ...
+; <id>         ::= <character>+
+; <plus>       ::= "{" "+" <expression> <expression> "}"
+; <multiply>   ::= "{" "*" <expression> <expression> "}"
+; <binding>    ::= "[" <id> <expression> "]"
+; <let>        ::= "{" "let" "{" <binding>* "}" <expression> "}"
+; <let*>       ::= "{" "let*" "{" <binding>* "}" <expression> "}"
+; <lambda>     ::= "{" "lambda" "{" <id>* "}" <expression> "}"
+; <call>       ::= "{" <id> <expression> "}"
 
 ;; parse ----------------------------------------
 ; S-Exp -> Curly
@@ -83,25 +94,32 @@
     [(s-exp-match? `SYMBOL s) (idE (s-exp->symbol s))]
     [(s-exp-match? `{+ ANY ANY} s)
      (plusE (parse (second (s-exp->list s)))
-            (parse (third (s-exp->list s))))]
+            (parse (third  (s-exp->list s))))]
     [(s-exp-match? `{* ANY ANY} s)
      (multE (parse (second (s-exp->list s)))
-            (parse (third (s-exp->list s))))]
-    [(s-exp-match? `{let {[SYMBOL ANY]} ANY} s)
-     (let ([bs (s-exp->list (first
-                             (s-exp->list (second
-                                           (s-exp->list s)))))])
-       (letE (s-exp->symbol (first bs))
-             (parse (second bs))
+            (parse (third  (s-exp->list s))))]
+    [(s-exp-match? `{let {[SYMBOL ANY] ...} ANY} s)
+     (let ([bindings (s-exp->list (second (s-exp->list s)))])
+       (letE (map s-exp->symbol (map first (map s-exp->list bindings)))
+             (map parse (map second (map s-exp->list bindings)))
              (parse (third (s-exp->list s)))))]
-    [(s-exp-match? `{lambda {SYMBOL} ANY} s)
+    [(s-exp-match? `{let* {[SYMBOL ANY] ...} ANY} s)
+     (let ([bindings (s-exp->list (second (s-exp->list s)))])
+       (letE* (map s-exp->symbol (map first (map s-exp->list bindings)))
+              (map parse (map second (map s-exp->list bindings)))
+              (parse (third (s-exp->list s)))))]
+    [(s-exp-match? `{lambda {SYMBOL ...} ANY} s)
      (lamE (s-exp-list->symbol-list (s-exp->list (second (s-exp->list s))))
-           (parse (third (s-exp->list s))))]
-    [(s-exp-match? `{ANY ANY} s)
-     (appE (parse (first (s-exp->list s)))
-           (parse (second (s-exp->list s))))]
+           (parse (third  (s-exp->list s))))]
+    [(s-exp-match? `{ANY ANY ...} s)
+     (appE (parse (first  (s-exp->list s)))
+           (let ([arg-list (rest (s-exp->list s))])
+             (cond
+               [(empty? arg-list) empty]
+               [(cons?  arg-list) (map parse arg-list)])))]
     [else (error 'parse "invalid input")]))
 
+;; TESTS parse
 (module+ test
   (test (parse `2)
         (numE 2))
@@ -114,45 +132,123 @@
   (test (parse `{+ {* 3 4} 8})
         (plusE (multE (numE 3) (numE 4))
                (numE 8)))
-  (test (parse `{let {[x {+ 1 2}]}
-                  y})
-        (letE 'x (plusE (numE 1) (numE 2))
+  
+  (test (parse `{let {} 
+                  y}) ; 0 bindings
+        (letE empty empty
               (idE 'y)))
-  (test (parse `{lambda {x} 9})
-        (lamE (list 'x) (numE 9)))
-  (test (parse `{double 9})
-        (appE (idE 'double) (numE 9)))
-  (test/exn (parse `{{+ 1 2}})
-            "invalid input"))
+  (test (parse `{let {[x {+ 1 2}]} 
+                  y}) ; 1 binding
+        (letE (list 'x) (list (plusE (numE 1) (numE 2)))
+              (idE 'y)))
+  
+  (test (parse `{let* {[z {+ 1 2}] [x {* 2 1}]}
+                  y}) ; multiple bindings
+        (letE* (list 'z 'x)
+               (list (plusE (numE 1) (numE 2)) (multE (numE 2) (numE 1)))
+               (idE 'y)))
+  
+  (test (parse `{lambda {} 9})      ; 0 arguments
+        (lamE empty (numE 9)))
+  (test (parse `{lambda {x} {* 3 x}})     ; 1 argument
+        (lamE (list 'x) (multE (numE 3) (idE 'x))))
+  (test (parse `{lambda {x y z} 9}) ; multiple arguments
+        (lamE (list 'x 'y 'z) (numE 9)))
+  
+  (test (parse `{double})           ; 0 arguments
+        (appE (idE 'double) empty))
+  (test (parse `{double 9})         ; 1 argument
+        (appE (idE 'double) (list (numE 9))))
+  (test (parse `{double 9 10 11})   ; multiple arguments
+        (appE (idE 'double) (list (numE 9) (numE 10) (numE 11))))
+  (test/exn (parse `{})
+            "invalid input")
+  ; intergration
+  (test (parse `{let {[x 1]}
+                  {let {[add-x {lambda {y} {+ x y}}]
+                        [x 2]}
+                    {add-x 3}}})
+        (letE (list 'x)
+              (list (numE 1))
+              (letE (list 'add-x 'x)
+                    (list (lamE (list 'y) (plusE (idE 'x) (idE 'y))) (numE 2))
+                    (appE (idE 'add-x) (list (numE 3))))))
+  (test (parse `{let {[x 1]}
+                  {let* {[x 2]
+                         [time-x {lambda {y} {* x y}}]}
+                    {time-x 3}}})
+        (letE (list 'x)
+              (list (numE 1))
+              (letE* (list 'x 'time-x)
+                     (list (numE 2) (lamE (list 'y) (multE (idE 'x) (idE 'y))))
+                     (appE (idE 'time-x) (list (numE 3)))))))
+
+; map1+ : just like "map" but takes one extra argument for convenience
+(define (map1+ [function : (Exp Env -> Value)]
+               [exps     : (Listof Exp)]
+               [env      : Env]) : (Listof Value)
+  (type-case (Listof Exp) exps
+    [empty empty]
+    [(cons exp rst-exps) (cons (function exp env) (map1+ function rst-exps env))]))
+
+; map1* : just like "map1+" but returns a list of lists of values
+; given: some-func '(1 2 3) some-env, expected: '('(list 1 2 3) '(2 3) '(3))
+(define (map1* [function : (Exp Env -> Value)]
+               [exps     : (Listof Exp)]
+               [env      : Env]) : (Listof (Listof Value))
+  (cond
+    [(empty? exps) empty]
+    [(cons? exps) (append (list (map1+ function exps env)) (map1* function (rest exps) env))]))
+
+; map2* : just like "map2" but takes an element and a list
+; apply the function on the element and each element of the list
+; return a new list
+(define (bind-list [id : Symbol] [values : (Listof Value)]) : (Listof Binding)
+  (type-case (Listof Value) values
+    [empty empty]
+    [(cons val rst-vals) (cons (bind id val) (bind-list id rst-vals))]))
+
+; flat-map : flatten a list
+; given: '('(1) '(2 3)), expected: '(1 2 3)
+(define (flat-map [a : (Listof (Listof Binding))]) : (Listof Binding)
+  (type-case (Listof (Listof Binding)) a
+    [empty empty]
+    [(cons bs rst-bs) (append bs (flat-map rst-bs))]))
+  
 
 ;; interp ----------------------------------------
 (define (interp [a : Exp] [env : Env]) : Value
   (type-case Exp a
     [(numE n) (numV n)]
-    [(idE s) (lookup s env)]
+    [(idE s)  (lookup s env)]
     [(plusE l r) (num+ (interp l env) (interp r env))]
     [(multE l r) (num* (interp l env) (interp r env))]
-    [(letE n rhs body) (interp body
-                               (extend-env
-                                (bind (list n) (interp rhs env))
-                                env))]
-    [(lamE n body) (closV n body env)]
-    [(appE fun arg) (type-case Value (interp fun env)
-                      [(closV n body c-env)
-                       (interp body
-                               (extend-env
-                                (bind n
-                                      (interp arg env))
-                                c-env))]
-                      [else (error 'interp "not a function")])]))
+    [(letE  ids exps body) (interp body
+                                   (append
+                                    (map2 bind ids (map1+ interp exps env))
+                                    env))]
+    [(letE* ids exps body) (interp body
+                                   (append
+                                    (flat-map (map2 bind-list ids (map1* interp exps env)))
+                                    env))]
+    [(lamE args body) (closV args body env)]
+    [(appE fun argsE) (type-case Value (interp fun env)
+                        [(closV argsV body c-env)
+                         (interp body
+                                 (append
+                                  (map2 bind argsV
+                                        (list (interp (first argsE) env))) ; TODO how to (interp exps env) foldl only takes <element> <list>
+                                  c-env))]
+                        [else (error 'interp "not a function")])]))
 
+; TESTS interp
 (module+ test
   (test (interp (parse `2) mt-env)
         (numV 2))
   (test/exn (interp (parse `x) mt-env)
             "free variable")
   (test (interp (parse `x) 
-                (extend-env (bind (list 'x) (numV 9)) mt-env))
+                (extend-env (bind 'x (numV 9)) mt-env))
         (numV 9))
   (test (interp (parse `{+ 2 1}) mt-env)
         (numV 3))
@@ -164,20 +260,29 @@
   (test (interp (parse `{lambda {x} {+ x x}})
                 mt-env)
         (closV (list 'x) (plusE (idE 'x) (idE 'x)) mt-env))
+  (test (interp (parse `{lambda {x y z} {* x {+ y z}}})
+                mt-env)
+        (closV (list 'x 'y 'z) (multE (idE 'x) (plusE (idE 'y) (idE 'z))) mt-env))
   (test (interp (parse `{let {[x 5]}
                           {+ x x}})
                 mt-env)
         (numV 10))
+  (test (interp (parse `{let* {[x 2]
+                               [time-x {lambda {y} {* 3 y}}]}
+                          {time-x 3}})
+                mt-env)
+        (numV 9))
   (test (interp (parse `{let {[x 5]}
                           {let {[x {+ 1 x}]}
                             {+ x x}}})
                 mt-env)
         (numV 12))
-  (test (interp (parse `{let {[x 5]}
-                          {let {[y 6]}
-                            x}})
+  (test (interp (parse `{let {[x 1]}
+                          {let* {[x 2]
+                                 [add-x {lambda {y} {+ x y}}]}
+                            {add-x 3}}})
                 mt-env)
-        (numV 5))
+        (numV 4))
   (test (interp (parse `{{lambda {x} {+ x x}} 8})
                 mt-env)
         (numV 16))
@@ -213,6 +318,7 @@
 (define (num* [l : Value] [r : Value]) : Value
   (num-op * l r))
 
+; TESTS num+ and num*
 (module+ test
   (test (num+ (numV 1) (numV 2))
         (numV 3))
@@ -224,20 +330,21 @@
   (type-case (Listof Binding) env
    [empty (error 'lookup "free variable")]
    [(cons b rst-env) (cond
-                       [(contains? n (bind-name b))
+                       [(symbol=? n (bind-name b))
                         (bind-val b)]
                        [else (lookup n rst-env)])]))
 
+; TESTS lookup
 (module+ test
   (test/exn (lookup 'x mt-env)
             "free variable")
-  (test (lookup 'x (extend-env (bind (list 'x) (numV 8)) mt-env))
+  (test (lookup 'x (extend-env (bind 'x (numV 8)) mt-env))
         (numV 8))
   (test (lookup 'x (extend-env
-                    (bind (list 'x) (numV 9))
-                    (extend-env (bind (list 'x) (numV 8)) mt-env)))
+                    (bind 'x (numV 9))
+                    (extend-env (bind 'x (numV 8)) mt-env)))
         (numV 9))
   (test (lookup 'y (extend-env
-                    (bind (list 'x) (numV 9))
-                    (extend-env (bind (list 'y) (numV 8)) mt-env)))
+                    (bind 'x (numV 9))
+                    (extend-env (bind 'y (numV 8)) mt-env)))
         (numV 8)))
